91fa859782450edbae9766a3cf65dac6cbd3354b
bri3ll3 <biacra2@alu.edu.gva.es>
2026-03-02 09:18:50 +0100

    refactor: simplified transformCodeToLetter using replace instead of split

--- src/Game.ts
+++ src/Game.ts
@@ -72,11 +72,11 @@ export class Game {
         return code=="Backspace";
     }
 
-    transformCodeToLetter(code: string):string{
-        let letter: string = "";
-        if (code=="Semicolon") letter = "Ñ";
-        else letter = code.split("y")[1];
-        return letter;
+    transformCodeToLetter(code: string): string {
+    if (code === "Semicolon") return "Ñ";
+    return code.startsWith("Key")
+        ? code.replace("Key", "").toUpperCase()
+        : "";
     }
 
     newLetter(code: string):void{
