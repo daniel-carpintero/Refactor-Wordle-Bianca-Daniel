3611110873f882d16fa8002b85293ea101cd9ac5
AzureAD\DanielCarpinteroGarc <daniel.carpintero@cuatroochenta.com>
2026-03-03 10:08:15 +0100

    refactor: replace multi-method evaluation with three-pass and remove unused methods

--- src/WordEvaluator.ts
+++ src/WordEvaluator.ts
@@ -4,47 +4,27 @@ import {Interface} from "./Interface.js";
 export type LetterResult = "right" | "misplaced" | "wrong" | null;
 
 export class WordEvaluator {
-    checkRightLetters (_pickedWord: string, _actualWord: string, results: LetterResult[]): void {
-        for(let i=0; i<MAX_WORD_SIZE; i++){
-            if (_pickedWord[i]==_actualWord[i]){
+    evaluateWord(pickedWord: string, actualWord: string): LetterResult[] {
+        const results: LetterResult[] = new Array(MAX_WORD_SIZE).fill("wrong");
+
+        const pickedLetters = pickedWord.split("");
+
+        for(let i = 0; i < MAX_WORD_SIZE; i++){
+            if(actualWord[i] === pickedLetters[i]){
                 results[i] = "right";
+                pickedLetters[i] = "";
             }
         }
-    }
 
-    checkMisplacedLetters = (_pickedWord: string, _actualWord: string, results: LetterResult[]):void=> {
-        let actualLetter: string = "";
-        let pattern: RegExp;
-        let numberOfCoincidences: number = 0;
-        let isMisplacedLetter: boolean;
-        for (let i=0; i<MAX_WORD_SIZE; i++){
-            isMisplacedLetter = true;
-            actualLetter = _actualWord[i];
-            pattern = new RegExp(actualLetter,"g");
-            numberOfCoincidences = (_pickedWord.match(pattern)||[]).length;
-            if (_pickedWord[i]==_actualWord[i]) isMisplacedLetter=false;
-            if (numberOfCoincidences>0 && isMisplacedLetter) results[i] = "misplaced";
-            
+        for(let i = 0; i < MAX_WORD_SIZE; i++){
+            if(results[i] !== "right"){
+                const indexInPicked = pickedLetters.indexOf(actualWord[i]);
+                if(indexInPicked !== -1){
+                    results[i] = "misplaced";
+                    pickedLetters[indexInPicked] = "";
+                }
+            }
         }
-    }
-
-    checkWrongLetters = (_pickedWord: string, _actualWord: string, results: LetterResult[]):void=>{
-        let actualLetter = "";
-        let pattern:RegExp;
-        let numberOfCoincidences = 0;
-        for (let i=0; i<MAX_WORD_SIZE; i++){
-            actualLetter = _actualWord[i];
-            pattern = new RegExp(actualLetter,"g");
-            numberOfCoincidences = (_pickedWord.match(pattern)||[]).length;
-            if (numberOfCoincidences==0) results[i] = "wrong";
-        }
-    }
-
-    evaluateWord(pickedWord: string, actualWord: string): LetterResult[]{
-        const results: LetterResult[] = new Array(MAX_WORD_SIZE).fill(null);
-        this.checkRightLetters(pickedWord, actualWord, results);
-        this.checkMisplacedLetters(pickedWord, actualWord, results);
-        this.checkWrongLetters(pickedWord, actualWord, results);
 
         return results;
     }
