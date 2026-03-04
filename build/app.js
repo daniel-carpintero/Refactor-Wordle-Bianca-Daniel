8a2b717a4a12ed79596343c00d96047bf3bb2da7
bri3ll3 <biacra2@alu.edu.gva.es>
2026-03-04 11:36:44 +0100

    fix: resolve merge conflict in build/script.js

--- build/app.js
+++ build/app.js
@@ -6,7 +6,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "..", "public")));
-//app.use(express.static(path.join(__dirname, "..", "src")));
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
