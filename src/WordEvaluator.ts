0cde948647c5025c090e57d72cd21e38e38f8287
AzureAD\DanielCarpinteroGarc <daniel.carpintero@cuatroochenta.com>
2026-03-04 14:47:15 +0100

    refactor: inject maxWordSize to remove dependency on env configuration

--- src/WordEvaluator.ts
+++ src/WordEvaluator.ts
@@ -1,21 +1,23 @@
-import {MAX_WORD_SIZE} from "./env.js";
 import { IWordEvaluator } from "./IWordEvaluator.js";
 
 export type LetterResult = "right" | "misplaced" | "wrong" | null;
 
 export class WordEvaluator implements IWordEvaluator{
+    
+    constructor(private readonly maxWordSize: number){}
+    
     evaluateWord(pickedWord: string, currentWord: string): LetterResult[] {
-        const results: LetterResult[] = new Array(MAX_WORD_SIZE).fill("wrong");
+        const results: LetterResult[] = new Array(this.maxWordSize).fill("wrong");
         const pickedLetters = pickedWord.split("");
 
-        for (let i = 0; i < MAX_WORD_SIZE; i++) {
+        for (let i = 0; i < this.maxWordSize; i++) {
             if (currentWord[i] === pickedLetters[i]) {
                 results[i] = "right";
                 pickedLetters[i] = "";
             }
         }
 
-        for (let i = 0; i < MAX_WORD_SIZE; i++) {
+        for (let i = 0; i < this.maxWordSize; i++) {
             if (results[i] !== "right") {
                 const indexInPicked = pickedLetters.indexOf(currentWord[i]);
                 if (indexInPicked !== -1) {
